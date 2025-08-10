6dac7021c5388d021087a11cf8577789379cb04a
SiddhantArorasvam <sidmee08@gmail.com>
2025-08-10 10:44:04 +0530

    first commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,23 @@
+public class Employee {
+    
+    int id;
+    String name;
+    double salary;
+    
+    
+    Employee(int id,String name,double salary){
+    
+    
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ src/main/java/EmployeeFilter.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class EmployeeFilter {
+    
+    public static void main(String[] args) {
+        
+        
+        List<Employee> employeeList = Arrays.asList(
+                new Employee(1,"Alice", 20000),
+                new Employee(2,"Angel", 70000),
+                new Employee(3,"Payal", 90000)
+                
+                
+        );
+        
+        employeeList.stream()
+                .filter(emp -> emp.salary >50000)
+                .sorted((e1,e2)-> Double.compare(e2.salary , e1.salary))
+                .map(emp -> emp.name)
+                .forEach(System.out::println);
+    }
+}
