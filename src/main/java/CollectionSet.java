5951c16d8cb29fc87589251d601ed47f89489780
SiddhantArorasvam <sidmee08@gmail.com>
2025-08-10 12:42:34 +0530

    this is SET

---
+++ src/main/java/CollectionSet.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class CollectionSet {
+    
+    public static void main(String[] args) {
+        //Collection Set -> No LOAD -> unordered , Unique Elements
+        
+        Set<String> countries = new HashSet<>();
+        countries.add("India");
+        countries.add("Netherland");
+        countries.add("Ibiza");
+        
+       for(String country : countries){
+           
+           System.out.println(country);
+       }
+        
+        
+    }
+}
