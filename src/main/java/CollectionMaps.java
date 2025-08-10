94adf3beacf4ff01af2fa71f55c7f4a1abe7f9ee
SiddhantArorasvam <sidmee08@gmail.com>
2025-08-10 13:15:17 +0530

    maps are here

---
+++ src/main/java/CollectionMaps.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionMaps {
+    
+    public static void main(String[] args) {
+        
+        Map<Integer,String> maps = new HashMap<>();
+        maps.put(1,"Siddhant");
+        maps.put(2,"Arora");
+        maps.put(3,"Siddhant");
+        
+        
+        //Iterate via KeySet
+        
+        for (Integer key : maps.keySet()){
+            
+            System.out.println(key +" -> "+maps.get(key));
+        }
+        
+        //Iterate via Entryset
+        
+        for (Map.Entry<Integer,String> entry : maps.entrySet()){
+            System.out.println(entry.getKey()+" -> "+entry.getValue());
+        }
+        
+    }
+}
